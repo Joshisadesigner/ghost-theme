99ade888400da71af455fed58e0357002bda5884
Josh De La Cruz <joshisadesigner@gmail.com>
2016-06-26 20:44:35 -0600

    Working on post page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,21 @@
 $( document ).ready( function() {
 
+    // HAMBURGER ICON ANIMATION
+    $('.sb__hamb').click(function() {
+        $(this).toggleClass('open');
+        $('.sb__mobile').toggle();
+    });
+
+    window.addEventListener('resize', function(event){
+        if( $( window ).width() > 739 ) {
+            $( '.sb__mobile').css( "display", "block" );
+            $('.sb__hamb').removeClass('open');
+        }
+        else {
+            $( '.sb__mobile').css( "display", "none" );
+        }
+    });
+
     // FRONT PAGE TAGS TOOLTIPS
     $( '.crd__tags-toggler' ).click( function() {
         var e = $( this ).find( '.crd__tags' ),
@@ -15,5 +31,5 @@ $( document ).ready( function() {
             e.addClass( 'show-tags' );
             $( this ).addClass( 'active' );
         }
-    })
+    });
 })
